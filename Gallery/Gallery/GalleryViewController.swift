9a5d20c2865fe84e088c22fec9919568a28ce0e5
Aleksei <82637156+Lioxaxa@users.noreply.github.com>
2024-01-08 20:20:25 +0300

    Project created

---
+++ Gallery/Gallery/GalleryViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gallery
+//
+//  Created by Pupko, Aleksey on 8.01.24.
+//
+
+import UIKit
+
+class GalleryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
